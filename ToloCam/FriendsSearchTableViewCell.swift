2625619c68b5d4ebc9401d414a52d7f8a332d66f
Leo <Chicco@chiccos-macbook-air.local>
2016-04-22 21:29:22 -0700

    Uploading file with profile completed

---
+++ ToloCam/FriendsSearchTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  FriendsSearchTableViewCell.swift
+//  ToloCam
+//
+//  Created by Federico Li on 4/22/16.
+//  Copyright © 2016 Federico Li. All rights reserved.
+//
+
+import UIKit
+
+class FriendsSearchTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
